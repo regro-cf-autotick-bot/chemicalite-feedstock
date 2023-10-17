26d259268ea541e87a1db338216a4071e2aaa198
Riccardo Vianello <riccardo.vianello@gmail.com>
2023-10-17 23:26:56 +0200

    workaround for old macOS sdk

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,12 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
   sed -i 's/ -Werror//' "$SRC_DIR/CMakeLists.txt"
 fi
 
+# workaround for old macOS sdk
+# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+if [[ "$target_platform" == osx-64 ]]; then
+    export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake ${CMAKE_ARGS} \
     -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D CMAKE_INSTALL_LIBDIR=lib \
