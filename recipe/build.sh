f9cc6c9974ccab63529e0faee9573d3c75210b2b
Riccardo Vianello <riccardo.vianello@gmail.com>
2024-02-13 09:54:29 +0100

    update for v2024.02.1 (#18)
    * update for v2024.02.1
    
    * unpin catch2
    
    * MNT: Re-rendered with conda-build 3.28.4, conda-smithy 3.30.4, and conda-forge-pinning 2024.02.08.22.31.48
    
    * don't run ctest on osx-arm64
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,25 @@
+#!/bin/bash
+
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
   sed -i "/INTERFACE_INCLUDE_DIRECTORIES/c\  INTERFACE_INCLUDE_DIRECTORIES \"$PREFIX/include/rdkit;$PREFIX/include\"" "$PREFIX/lib/cmake/rdkit/rdkit-targets.cmake"
   sed -i 's/ -Werror//' "$SRC_DIR/CMakeLists.txt"
 fi
 
-# workaround for old macOS sdk
-# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-if [[ "$target_platform" == osx-64 ]]; then
-    export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
+if [[ "$target_platform" == osx-arm64 ]] || [[ "$target_platform" == osx-64 ]]; then
+    export CXXFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY -D_HAS_AUTO_PTR_ETC=0 $CXXFLAGS"
 fi
 
 cmake ${CMAKE_ARGS} \
     -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D CMAKE_INSTALL_LIBDIR=lib \
     -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_CATCH_DISCOVER_TESTS_DISCOVERY_MODE=PRE_TEST \
     $SRC_DIR
 
 make
 
-LD_LIBRARY_PATH=$PWD/src ctest --output-on-failure
+if [[ "$target_platform" != osx-arm64 ]]; then
+    LD_LIBRARY_PATH=$PWD/src ctest --output-on-failure
+fi
 
 make install
